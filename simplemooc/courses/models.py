86971d7bc0a222645a2ee4a45d4c42dc7932a063
Patrick Mazulo <pmazulo@gmail.com>
2014-05-15 17:21:38 -0300

    "Alteração na retirada de redudância (PEP)"

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -5,7 +5,7 @@ class CourseManager(models.Manager):
 
     def search(self, query):
         return self.get_queryset().filter(
-            models.Q(name__icontains=query) | \
+            models.Q(name__icontains=query) |
             models.Q(description__icontains=query)
         )
 
