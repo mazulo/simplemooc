1266c9b0e2e10e4e03fa738e8501803c46eb9722
Patrick Mazulo <pmazulo@gmail.com>
2015-06-01 11:39:50 -0300

    Static files

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -115,9 +115,6 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 try:
     from simplemooc.local_settings import *
