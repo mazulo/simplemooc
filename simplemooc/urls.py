dbc11df0cc51241909bb6733ddca3d81502acb4d
Patrick Mazulo <pmazulo@gmail.com>
2016-04-11 16:19:37 -0300

    Another changes

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -1,19 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^', include('simplemooc.core.urls', namespace='core')),
     url(r'^cursos/', include('simplemooc.courses.urls', namespace='courses')),
     url(r'^contas/', include(
         'simplemooc.accounts.urls', namespace='accounts')),
     url(r'^forum/', include('simplemooc.forum.urls', namespace='forum')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 if settings.DEBUG:
     urlpatterns += static(
