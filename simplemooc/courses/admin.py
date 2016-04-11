777f1fdb663453e9f830c6522d196553f86ef506
Patrick Mazulo <pmazulo@gmail.com>
2016-04-10 23:05:53 -0300

    Add new classes in admin

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import (
-    Course, Announcement, Comment, Enrollment, Lesson, Material
+    Course,
+    CourseTRB,
+    Announcement,
+    Comment,
+    Enrollment,
+    Lesson,
+    LessonTRB,
+    Material,
+    KnowledgeLevel,
+    CategoryDimensionCognitiveProcess,
+    Verb,
 )
 
 
@@ -10,6 +20,10 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'start_date', 'created_at']
     search_fields = ['name', 'slug']
     prepopulated_fields = {'slug': ['name']}
+
+
+class CourseTRBAdmin(CourseAdmin):
+    pass
 
 
 class MaterialInlineAdmin(admin.StackedInline):
@@ -26,6 +40,12 @@ class LessonAdmin(admin.ModelAdmin):
         MaterialInlineAdmin
     ]
 
+
 admin.site.register(Course, CourseAdmin)
+admin.site.register(CourseTRB, CourseTRBAdmin)
 admin.site.register([Enrollment, Announcement, Comment, Material])
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(LessonTRB, LessonAdmin)
+admin.site.register(KnowledgeLevel)
+admin.site.register(CategoryDimensionCognitiveProcess)
+admin.site.register(Verb)
