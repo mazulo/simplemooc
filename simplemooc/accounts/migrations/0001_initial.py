ea0efc17ffab9ea012c5711e9ff79bc4d2947ea9
Patrick Mazulo <pmazulo@gmail.com>
2016-04-22 18:14:41 -0300

    Change migration file

--- simplemooc/accounts/migrations/0001_initial.py
+++ simplemooc/accounts/migrations/0001_initial.py
@@ -2,10 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import re
-import django.contrib.auth.models
 import django.core.validators
 from django.conf import settings
+import django.contrib.auth.models
+import re
 
 
 class Migration(migrations.Migration):
@@ -18,16 +18,16 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
                 ('password', models.CharField(verbose_name='password', max_length=128)),
                 ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
-                ('is_superuser', models.BooleanField(help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status', default=False)),
-                ('username', models.CharField(validators=[django.core.validators.RegexValidator(re.compile('^[\\w.@+-]+$', 32), 'O nome de usuário só pode conter letras, números e @/./+/-/_', 'invalid')], verbose_name='Nome de Usuário', max_length=30, unique=True)),
+                ('is_superuser', models.BooleanField(verbose_name='superuser status', default=False, help_text='Designates that this user has all permissions without explicitly assigning them.')),
+                ('username', models.CharField(verbose_name='Nome de Usuário', validators=[django.core.validators.RegexValidator(re.compile('^[\\w.@+-]+$', 32), 'O nome de usuário só pode conter letras, números e @/./+/-/_', 'invalid')], max_length=30, unique=True)),
                 ('email', models.EmailField(verbose_name='E-mail', max_length=254, unique=True)),
                 ('name', models.CharField(verbose_name='Nome', max_length=100, blank=True)),
                 ('is_active', models.BooleanField(verbose_name='Está ativo?', default=True)),
                 ('is_staff', models.BooleanField(verbose_name='É da equipe?', default=False)),
-                ('date_joined', models.DateTimeField(auto_now_add=True, verbose_name='Data de entrada')),
+                ('date_joined', models.DateTimeField(verbose_name='Data de entrada', auto_now_add=True)),
             ],
             options={
                 'verbose_name': 'Usuário',
@@ -40,21 +40,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PasswordReset',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
                 ('key', models.CharField(verbose_name='Chave', max_length=100, unique=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Criado em')),
+                ('created_at', models.DateTimeField(verbose_name='Criado em', auto_now_add=True)),
                 ('confirmed', models.BooleanField(verbose_name='Confirmado?', default=False)),
             ],
             options={
+                'ordering': ['-created_at'],
                 'verbose_name': 'Nova senha',
                 'verbose_name_plural': 'Novas senhas',
-                'ordering': ['-created_at'],
             },
         ),
         migrations.CreateModel(
             name='Professor',
             fields=[
-                ('user_ptr', models.OneToOneField(parent_link=True, serialize=False, primary_key=True, auto_created=True, to=settings.AUTH_USER_MODEL)),
+                ('user_ptr', models.OneToOneField(to=settings.AUTH_USER_MODEL, primary_key=True, parent_link=True, auto_created=True, serialize=False)),
             ],
             options={
                 'verbose_name': 'professor',
@@ -65,16 +65,16 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='passwordreset',
             name='user',
-            field=models.ForeignKey(related_name='resets', verbose_name='Usuário para recuperação de senha', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(verbose_name='Usuário para recuperação de senha', to=settings.AUTH_USER_MODEL, related_name='resets'),
         ),
         migrations.AddField(
             model_name='user',
             name='groups',
-            field=models.ManyToManyField(related_query_name='user', blank=True, related_name='user_set', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups', to='auth.Group'),
+            field=models.ManyToManyField(verbose_name='groups', related_query_name='user', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', to='auth.Group', blank=True),
         ),
         migrations.AddField(
             model_name='user',
             name='user_permissions',
-            field=models.ManyToManyField(related_query_name='user', blank=True, related_name='user_set', help_text='Specific permissions for this user.', verbose_name='user permissions', to='auth.Permission'),
+            field=models.ManyToManyField(verbose_name='user permissions', related_query_name='user', help_text='Specific permissions for this user.', related_name='user_set', to='auth.Permission', blank=True),
         ),
     ]
