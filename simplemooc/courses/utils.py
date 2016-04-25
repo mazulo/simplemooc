4b320a5ea14d94278f3414d2cecd7912eb1e2997
Patrick Mazulo <pmazulo@gmail.com>
2016-04-25 17:33:32 -0300

    Add get_course_by_instance function helper

--- simplemooc/courses/utils.py
+++ simplemooc/courses/utils.py
@@ -2,3 +2,11 @@ def format_name(name):
     if name.count('/'):
         return name.replace('/', '_').lower()
     return name.lower()
+
+
+def get_course_by_instance(course_slug, Course, CourseTRB):
+    try:
+        course = Course.objects.get(slug=course_slug)
+    except Course.DoesNotExist:
+        course = CourseTRB.objects.get(slug=course_slug)
+    return course
