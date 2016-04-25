101631984c148f35b7d0a528b37d4eee2bd5f848
Patrick Mazulo <pmazulo@gmail.com>
2016-04-25 17:34:17 -0300

    Change migration file

--- simplemooc/forum/migrations/0001_initial.py
+++ simplemooc/forum/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 import taggit.managers
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
@@ -17,42 +17,42 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Reply',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('reply', models.TextField(verbose_name='Resposta')),
-                ('correct', models.BooleanField(verbose_name='Correta?', default=False)),
+                ('correct', models.BooleanField(default=False, verbose_name='Correta?')),
                 ('created', models.DateTimeField(auto_now_add=True, verbose_name='Criado em')),
                 ('modified', models.DateTimeField(auto_now=True, verbose_name='Modificado em')),
-                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='replies', verbose_name='Autor')),
+                ('author', models.ForeignKey(verbose_name='Autor', related_name='replies', to=settings.AUTH_USER_MODEL)),
             ],
             options={
-                'ordering': ['-correct', 'created'],
                 'verbose_name_plural': 'Respostas',
+                'ordering': ['-correct', 'created'],
                 'verbose_name': 'Resposta',
             },
         ),
         migrations.CreateModel(
             name='Thread',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
-                ('title', models.CharField(verbose_name='Título', max_length=100)),
-                ('slug', models.SlugField(unique=True, verbose_name='Identificador', max_length=100)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('title', models.CharField(max_length=100, verbose_name='Título')),
+                ('slug', models.SlugField(max_length=100, unique=True, verbose_name='Identificador')),
                 ('body', models.TextField(verbose_name='Mensagem')),
-                ('views', models.IntegerField(verbose_name='Visualizações', default=0, blank=True)),
-                ('answers', models.IntegerField(verbose_name='Respostas', default=0, blank=True)),
+                ('views', models.IntegerField(blank=True, default=0, verbose_name='Visualizações')),
+                ('answers', models.IntegerField(blank=True, default=0, verbose_name='Respostas')),
                 ('created', models.DateTimeField(auto_now_add=True, verbose_name='Criado em')),
                 ('modified', models.DateTimeField(auto_now=True, verbose_name='Modificado em')),
-                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='threads', verbose_name='Autor')),
-                ('tags', taggit.managers.TaggableManager(help_text='A comma-separated list of tags.', to='taggit.Tag', verbose_name='Tags', through='taggit.TaggedItem')),
+                ('author', models.ForeignKey(verbose_name='Autor', related_name='threads', to=settings.AUTH_USER_MODEL)),
+                ('tags', taggit.managers.TaggableManager(help_text='A comma-separated list of tags.', to='taggit.Tag', through='taggit.TaggedItem', verbose_name='Tags')),
             ],
             options={
-                'ordering': ['-modified'],
                 'verbose_name_plural': 'Tópicos',
+                'ordering': ['-modified'],
                 'verbose_name': 'Tópico',
             },
         ),
         migrations.AddField(
             model_name='reply',
             name='thread',
-            field=models.ForeignKey(to='forum.Thread', related_name='replies', verbose_name='Tópico'),
+            field=models.ForeignKey(verbose_name='Tópico', related_name='replies', to='forum.Thread'),
         ),
     ]
