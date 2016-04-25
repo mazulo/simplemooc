7f20f67d70ef4351d838621191b3447893b604d3
Patrick Mazulo <pmazulo@gmail.com>
2016-04-25 17:29:50 -0300

    Update decorator to work with content_type relation

--- simplemooc/courses/decorators.py
+++ simplemooc/courses/decorators.py
@@ -1,18 +1,23 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 
-from .models import Course, Enrollment
+from .models import Course, CourseTRB, Enrollment
+from .utils import get_course_by_instance
 
 
 def enrollment_required(view_func):
     def _wrapper(request, *args, **kwargs):
         slug = kwargs['slug']
-        course = get_object_or_404(Course, slug=slug)
+        course = get_course_by_instance(slug, Course, CourseTRB)
+        content_type = ContentType.objects.get_for_model(course)
         has_permission = request.user.is_staff
         if not has_permission:
             try:
                 enrollment = Enrollment.objects.get(
-                    course=course, user=request.user
+                    content_type=content_type,
+                    object_id=course.id,
+                    user=request.user
                 )
             except Enrollment.DoesNotExist:
                 message = 'Desculpe, mas você não tem permissão de acesso'
