f31835c71b4e2f0045d8ff7db85b4613174bbb63
Patrick Mazulo <pmazulo@gmail.com>
2016-05-04 17:03:08 -0300

    Change description attr

--- simplemooc/courses/management/commands/_categories.py
+++ simplemooc/courses/management/commands/_categories.py
@@ -1,39 +1,26 @@
 CATEGORIES = [
     {
         'name': 'Lembrar',
-        'description': 'O aluno deverá distinguir e selecionar uma determinada\
-                        informação e reproduzir ou recordar uma informação\
-                        relevante memorizada.'
+        'description': 'O aluno deverá distinguir e selecionar uma determinada informação e reproduzir ou recordar uma informação relevante memorizada.'
     },
     {
         'name': 'Entender',
-        'description': 'O aluno deverá estabelecer uma conexão entre o novo\
-                        conhecimento e o conhecimento previamente adquirido.\
-                        Entender a informação e reproduzi-la com suas próprias\
-                        palavras.'
+        'description': 'O aluno deverá estabelecer uma conexão entre o novo conhecimento e o conhecimento previamente adquirido. Entender a informação e reproduzi-la com suas próprias palavras.'
     },
     {
         'name': 'Aplicar',
-        'description': 'O aluno deverá executar um procedimento numa\
-                        determinada situação ou abordar a aplicação de um\
-                        conhecimento em uma situação nova.'
+        'description': 'O aluno deverá executar um procedimento numa determinada situação ou abordar a aplicação de um conhecimento em uma situação nova.'
     },
     {
         'name': 'Analisar',
-        'description': 'O aluno deverá dividir a informação nas suas partes\
-                        constituintes e entender a relação de interdependência\
-                        existente entre elas.'
+        'description': 'O aluno deverá dividir a informação nas suas partes constituintes e entender a relação de interdependência existente entre elas.'
     },
     {
         'name': 'Avaliar',
-        'description': 'O aluno deverá realizar julgamentos baseados em padrões\
-                        e critérios específicos.'
+        'description': 'O aluno deverá realizar julgamentos baseados em padrões e critérios específicos.'
     },
     {
         'name': 'Criar',
-        'description': 'O aluno deverá colocar elementos juntos, com o\
-                        objetivo de criar um novo modelo ou estrutura,\
-                        uma nova solução, utilizando conhecimentos e\
-                        habilidades previamente adquiridos.'
+        'description': 'O aluno deverá colocar elementos juntos, com o objetivo de criar um novo modelo ou estrutura, uma nova solução, utilizando conhecimentos e habilidades previamente adquiridos.'
     },
 ]
--- simplemooc/courses/management/commands/_levels.py
+++ simplemooc/courses/management/commands/_levels.py
@@ -1,46 +1,18 @@
 LEVELS = [
     {
         'name': 'Efetivo/Factual',
-        'description': 'Este nível refere-se a fatos essenciais, terminologias,\
-                        detalhes ou elementos relacionados ao conteúdo básico\
-                        que o aluno deve dominar ou estar familiarizado a fim\
-                        de que consiga realizar e resolver problemas apoiados\
-                        nesse conhecimento ou compreender uma disciplina.\
-                        Relacionado aos fatos que não não precisam ser\
-                        entendidos ou combinados apenas reproduzidos como\
-                        apresentados.'
+        'description': 'Este nível refere-se a fatos essenciais, terminologias, detalhes ou elementos relacionados ao conteúdo básico que o aluno deve dominar ou estar familiarizado a fim de que consiga realizar e resolver problemas apoiados nesse conhecimento ou compreender uma disciplina. Relacionado aos fatos que não não precisam ser entendidos ou combinados apenas reproduzidos como apresentados.'
     },
     {
         'name': 'Conceitual',
-        'description': 'Elementos mais simples foram abordados e agora precisam\
-                        ser conectados. Este nível é relacionado à\
-                        inter-relação dos elementos básicos num contexto mais\
-                        elaborado que o aluno é capaz de descobrir. Inclui o\
-                        conhecimento das classificações, princípios,\
-                        generalizações, teorias, modelos ou estruturas\
-                        relacionadas a uma determinada área disciplinar.\
-                        Nessa fase, não é a aplicação de um modelo que é\
-                        importante, mas a consciência de sua existência.'
+        'description': 'Elementos mais simples foram abordados e agora precisam ser conectados. Este nível é relacionado à inter-relação dos elementos básicos num contexto mais elaborado que o aluno é capaz de descobrir. Inclui o conhecimento das classificações, princípios, generalizações, teorias, modelos ou estruturas relacionadas a uma determinada área disciplinar. Nessa fase, não é a aplicação de um modelo que é importante, mas a consciência de sua existência'
     },
     {
         'name': 'Procedural',
-        'description': 'Neste momento, o conhecimento abstrato começa a ser\
-                        estimulado "como realizar alguma coisa" utilizando\
-                        critérios, métodos e técnicas. Este nível refere-se à\
-                        informação ou conhecimento que ajuda o aluno a fazer\
-                        algo específico de uma disciplina, assunto ou área de\
-                        estudo. Inclui a forma de uma série ou sequência\
-                        de passos a serem seguidos e conhecimento de\
-                        critérios usados para determinar quando usar vários\
-                        procedimentos.'
+        'description': 'Neste momento, o conhecimento abstrato começa a ser estimulado "como realizar alguma coisa" utilizando critérios, métodos e técnicas. Este nível refere-se à informação ou conhecimento que ajuda o aluno a fazer algo específico de uma disciplina, assunto ou área de estudo. Inclui a forma de uma série ou sequência de passos a serem seguidos e conhecimento de critérios usados para determinar quando usar vários procedimentos.'
     },
     {
         'name': 'Metacognitivo',
-        'description': 'Reconhecimento da cognição em geral e da consciência\
-                        da amplitude e profundidade de conhecimento adquirido\
-                        sobre determinado conteúdo. Este conhecimento e\
-                        estratégico e reflexivo sobre como proceder para a\
-                        resolução de problemas e/ou escolha da melhor\
-                        metodologia, teoria ou estrutura para tal.'
+        'description': 'Reconhecimento da cognição em geral e da consciência da amplitude e profundidade de conhecimento adquirido sobre determinado conteúdo. Este conhecimento e estratégico e reflexivo sobre como proceder para a resolução de problemas e ou escolha da melhor metodologia, teoria ou estrutura para tal.'
     },
 ]
